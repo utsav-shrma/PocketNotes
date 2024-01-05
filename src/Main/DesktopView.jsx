c453d7c1cae9c7d82892207d7de40a0322a4cecb
Utsav Sharma <utsav19913@gmail.com>
2024-01-05 23:20:04 +0530

    added auto component view redirect

--- src/Main/DesktopView.jsx
+++ src/Main/DesktopView.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import { useDispatch } from "react-redux";
 import './DesktopView.css'
 import { setMobileView } from '../features/utility/utility';
-
+import { useNavigate } from 'react-router-dom';
 import ListGroups from "../Groups/ListGroups";
 import ListNotes from "../Notes/ListNotes";
 
 function DesktopView() {
     const dispatch=useDispatch();
     dispatch(setMobileView(false));
+    const navigate = useNavigate();
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  // Update windowWidth when the window is resized
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  // Attach event listener for window resize
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+
+    if(windowWidth<=800){
+      
+      navigate('/mobile');
+    }
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  });
     
   return (
     <div id="main-container">
--- src/Main/MobileView.jsx
+++ src/Main/MobileView.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import ListNotes from "../Notes/ListNotes";
 import ListGroups from "../Groups/ListGroups";
+import { useNavigate } from 'react-router-dom';
 
 import { useDispatch,useSelector } from "react-redux";
 import { setMobileView,setGroupView } from '../features/utility/utility';
@@ -9,6 +10,27 @@ function MobileView() {
     const dispatch=useDispatch();
     let isGroupView = useSelector((state) => state.utility.isGroupView);
     dispatch(setMobileView(true));
+
+    const navigate = useNavigate();
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  // Update windowWidth when the window is resized
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  // Attach event listener for window resize
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+
+    if(windowWidth>800){
+      navigate('/');
+    }
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  });
 
   return (
     <div>
